3990f9d9705d1ed81afdf5466d29d654c7e1d081
OwenDa <owendarcy25@gmail.com>
2022-07-21 16:15:09 +0000

    Update config following Heroku maintenance

--- operaireland/settings.py
+++ operaireland/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['opera-ireland.herokuapp.com']
+ALLOWED_HOSTS = ['opera-ireland.herokuapp.com', 'localhost']
 
 
 # Application definition
