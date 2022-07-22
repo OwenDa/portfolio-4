02aaed8eb0b4f7fcd4fd6a7aae0fb131a90f20c2
OwenDa <owendarcy25@gmail.com>
2022-07-22 11:34:07 +0000

    Add empty base+index.html templtes

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse('<h1>WelCome</h1>')
+    return render(request, 'index.html')
