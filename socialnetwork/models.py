e7073b596bdadc92ba5c60d2bb3ad2c16fa64109
OwenDa <owendarcy25@gmail.com>
2022-08-03 09:11:30 +0000

    Draft Profile model

--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -1,3 +1,20 @@
+""" Database models """
 from django.db import models
+from django.contrib.auth import get_user_model
+# from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    """ Extends built-in user model """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    iduser = models.IntegerField()
+    bio = models.TextField(max_length=280, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    avatar = models.ImageField(
+        'image', default='default-user.jpg', upload_to='avatars')
+
+    def __str__(self):
+        return self.user.username
