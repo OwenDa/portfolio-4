de5bc59481c0b49deaf0174473c61e4e0ae0343a
OwenDa <owendarcy25@gmail.com>
2022-08-05 12:31:11 +0000

    Add own-profile link to signed-in Nav

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -86,11 +86,11 @@ def profile(request, pk):
     based on primary key (pk), see urls.py
     """
     user_object = User.objects.get(username=pk)
-    # create variable 'user_object' by getting the user (object from Users db)
+    # create variable 'user_object' by getting the user (object in User table)
     # where username is equal to the pk passed in (ie. that used in the url)
     user_profile = Profile.objects.get(user=user_object)
     # create variable 'user_profile' by getting the profile (object from
-    # Profile db) in which the user is equal to the user_object variable above.
+    # Profile table) in which the user is equal to user_object variable above.
     # In effect, user_profile now refers to the profile for the pk passed in.
     context = {'user_object': user_object, 'user_profile': user_profile}
     # returning multiple variables, so using a context to bundle together.
