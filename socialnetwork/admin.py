6e81ae8c766dfc16442dff6ec1f48cbe14b8676c
OwenDa <owendarcy25@gmail.com>
2022-08-27 20:55:01 +0000

    Add ModelAdmin.list_display for all models

--- socialnetwork/admin.py
+++ socialnetwork/admin.py
@@ -2,9 +2,29 @@ from django.contrib import admin
 from .models import (Profile,
                      SocialLink, HistoryItem, Post, PostApplause)
 
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(SocialLink)
-admin.site.register(HistoryItem)
-admin.site.register(Post)
-admin.site.register(PostApplause)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'role', )
+
+
+class SocialLinkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'site_name', 'link', )
+
+
+class HistoryItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'history_heading', 'year', 'history_role', )
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'id',  'user', 'no_of_applause', )
+
+
+class PostApplauseAdmin(admin.ModelAdmin):
+    list_display = ('post_id', 'username', )
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(SocialLink, SocialLinkAdmin)
+admin.site.register(HistoryItem, HistoryItemAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostApplause, PostApplauseAdmin)
