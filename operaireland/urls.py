1bc0fb98ea7c0ac36e1e026f87cc1774cdeda409
OwenDa <owendarcy25@gmail.com>
2022-08-27 14:49:22 +0000

    Edit admin url

--- operaireland/urls.py
+++ operaireland/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('upper-circle/', admin.site.urls),
     path('', include('socialnetwork.urls')),
 ]
 
